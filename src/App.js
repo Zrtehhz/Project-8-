8e38dce6acd5db17b3c26c7c4eb6ddf400839e27
Zrtehhz <beugniesjimmy@gmail.com>
2023-09-10 15:02:22 +0200

    .
    Co-Authored-By: Jimmy Beugnies <93620368+zrtehhz@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 import { BrowserRouter as Router } from "react-router-dom"
+import Logo from './Assets/LOGO.svg';
+import background_top from './Assets/background_top.png';
 
 
 
@@ -11,11 +12,19 @@ function App() {
     <div className="App">
       <header className="App-header">
       <nav className="navbar">
-
-
+      <img className='img_navbar' src={Logo} />
+      <div className='right_section'>
+        <ul><li><a href="#" className='navbar_link'>Accueil</a></li></ul>
+        <ul><li><a href="#" className='navbar_link'>A Propos</a></li></ul>
+      </div>
       </nav>
       </header>
       <body>
+        <section className='top_body_section'>
+          <div class='top_body_div'>
+            <img className='img_body' src={background_top} />
+          </div>
+        </section>
 
       </body>
       <footer>
