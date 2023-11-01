f7d448d17f8a4929ffe4c2b875af7a575596d740
Jimmy Beugnies <93620368+Zrtehhz@users.noreply.github.com>
2023-11-01 21:18:25 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import About from './pages/about/about';
 import Error from './pages/error/error';
@@ -8,11 +8,11 @@ function App() {
     return (
       <Router>
         <div className="App">
-          <Switch>
+          <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
             <Route path="*" element={<Error />} />
-          </Switch>
+          </Routes>
         </div>
       </Router>
     );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Logo from './Assets/LOGO.svg';
+import Logo from '../../Assets/logo.png';
+import Navbar from '../../components/navbar/navbar'
 
 function Header() {
     return (
-<header className='header'>
+		<header className='header'>
 			<h1>
 				<img src={Logo} alt="location d'appartements" />
 			</h1>
@@ -12,4 +13,4 @@ function Header() {
     );
   }
 
-export default Header;
+  export default Header;
