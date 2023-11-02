a62907b3eb01930a92d238534c31d4f3efed35ce
Jimmy Beugnies <93620368+Zrtehhz@users.noreply.github.com>
2023-11-02 11:04:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,11 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-          <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-          </Routes>
-      </Router>
+<App />
   </React.StrictMode>
 );
 
