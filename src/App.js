a1e8bb298e9e9f876a9012cdddd8685cb5cbe691
Jimmy Beugnies <93620368+Zrtehhz@users.noreply.github.com>
2023-11-15 15:16:52 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/home';
+import Home from './pages/Home/Home';
 import About from './pages/about/about';
 import Error from './pages/error/error';
-import House from './pages/house/house';
+import House from './pages/house/House';
 
 function App() {
     return (
