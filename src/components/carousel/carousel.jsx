2e7cbaab67d17bb23b92b550648f3f17b1ca6306
Jimmy Beugnies <93620368+Zrtehhz@users.noreply.github.com>
2023-12-15 12:10:01 +0100

    .

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -6,21 +6,22 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 function Carousel({ logement }) {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    if (!logement || !logement.pictures || !logement.pictures.length) {
-        return <div>Chargement...</div>;
-    }
+// Fonction pour afficher l'image suivante dans le tableau de photos du logement
+const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+        // Vérifie si l'index actuel est le dernier dans le tableau, revient au début si vrai, sinon passe à l'index suivant
+        prevIndex === logement.pictures.length - 1 ? 0 : prevIndex + 1
+    );
+};
 
-    const nextImage = () => {
-        setCurrentImageIndex((prevIndex) =>
-            prevIndex === logement.pictures.length - 1 ? 0 : prevIndex + 1
-        );
-    };
+// Fonction pour afficher l'image précédente dans le tableau de photos du logement
+const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+        // Vérifie si l'index actuel est le premier dans le tableau, revient à la fin si vrai, sinon passe à l'index précédent
+        prevIndex === 0 ? logement.pictures.length - 1 : prevIndex - 1
+    );
+};
 
-    const prevImage = () => {
-        setCurrentImageIndex((prevIndex) =>
-            prevIndex === 0 ? logement.pictures.length - 1 : prevIndex - 1
-        );
-    };
 
     return (
         <main className="fiche-logement">
